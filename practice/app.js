74bd3d259a707b310291ee8c54b579891634c958
Zaroon <sib.zaroon@gmail.com>
2024-10-10 22:11:36 +0530

    final currency

--- practice/app.js
+++ practice/app.js
@@ -1,6 +1,8 @@
-const BASE_URL =
-  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+const BASE_URL = "https://latest.currency-api.pages.dev/v1/currencies";
 const dropDowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector(".btnCoverter");
+const fromCurn = document.querySelector(".from select");
+const toCurn = document.querySelector(".to select");
 
 for (let select of dropDowns) {
   for (currcode in countryList) {
@@ -25,4 +27,29 @@ const updateFlag = (element) => {
   let newSrc = `https://flagsapi.com/${countryCode}/shiny/32.png`;
   let img = element.parentElement.querySelector(".dropdown img");
   img.src = newSrc;
+};
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateRate();
+});
+window.addEventListener("load", () => {
+  updateRate();
+});
+
+const updateRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amountVal = amount.value;
+  if (amountVal === "" || amountVal < 1) {
+    amountVal = 1;
+    amount.value = "1";
+  }
+
+  let url = `${BASE_URL}/${fromCurn.value.toLowerCase()}.json`;
+  let response = await fetch(url);
+  let data = await response.json();
+  let rate = data[fromCurn.value.toLowerCase()][toCurn.value.toLowerCase()];
+  let finalAmount = amountVal * rate;
+  let msg = document.querySelector(".msg");
+  msg.innerText = `${amountVal} ${fromCurn.value} = ${finalAmount} ${toCurn.value}`;
 };
